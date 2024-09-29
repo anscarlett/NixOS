c8acc2af8a801b054c00a12a93a043a299da46c3
Skalamanga <anscarlett@gmail.com>
2024-09-29 21:06:33 +0100

    Update default.nix

--- homes/personal/adrian/default.nix
+++ homes/personal/adrian/default.nix
@@ -1,1 +1,9 @@
-# defaults for user
+{
+  imports = [ ./path/to/your/default.nix ];
+
+  zshConfig = {
+    promptInit = "prompt spaceship";
+    plugins = [ "zsh-completions" ];
+    historySize = 5000;
+  };
+}
