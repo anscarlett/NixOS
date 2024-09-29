9074660c40e4b355e2b90836bd8c16e85129dd36
Skalamanga <anscarlett@gmail.com>
2024-09-29 21:05:03 +0100

    Update default.nix

--- configs/shells/zsh/default.nix
+++ configs/shells/zsh/default.nix
@@ -1,1 +1,31 @@
-# defaults for zsh
+
+
+{ config, pkgs, ... }:
+
+let
+  zshConfig = {
+    promptInit = "prompt pure";
+    plugins = [ "git" "zsh-syntax-highlighting" "zsh-autosuggestions" ];
+    historySize = 10000;
+  };
+in
+{
+  home.packages = with pkgs; [
+    zsh
+  ];
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -al";
+      grep = "grep --color=auto";
+    };
+    initExtra = ''
+      ${config.zshConfig.promptInit}
+      for plugin in ${config.zshConfig.plugins}; do
+        source "$plugin"
+      done
+      HISTSIZE=${config.zshConfig.historySize}
+    '';
+  };
+}
